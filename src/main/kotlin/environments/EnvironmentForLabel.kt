4705b9c44a4488f753ebd38c29a12cf53e613ec5
IgorErin <erin.igore@yandex.ru>
2022-04-12 23:55:43 +0300

    added full parser

--- src/main/kotlin/environments/EnvironmentForLabel.kt
+++
@@ -1,4 +0,0 @@
-package environments
-
-class EnvironmentForLabel {
-}
--- src/main/kotlin/parser/statements/VarStatement.kt
+++
@@ -1,7 +0,0 @@
-package parser.statements
-
-class VarStatement(val name: String) : Statement {
-    override fun <T> accept(visitor: StmtVisitor<T>): T {
-        return visitor.visitVarStatement(this)
-    }
-}
