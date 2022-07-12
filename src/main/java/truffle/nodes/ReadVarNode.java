a2016e2bf7122f8527e573bbebf1e399662ac7e6
IgorErin <erin.igore@yandex.ru>
2022-07-12 12:41:03 +0300

    refactor: readNode

--- src/main/java/truffle/nodes/ReadVarNode.java
+++ src/main/java/truffle/nodes/ReadVarNode.java
@@ -2,41 +2,34 @@ package truffle.nodes;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.NodeField;
-import com.oracle.truffle.api.dsl.NodeFields;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.Frame;
-import com.oracle.truffle.api.frame.MaterializedFrame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
-@NodeFields(value = {
-        @NodeField(name = "slot", type = int.class),
-        @NodeField(name = "scope", type = MaterializedFrame.class)
-})
+@NodeField(name = "slot", type = int.class)
 public abstract class ReadVarNode {
     protected abstract int getSlot();
-    protected abstract Frame getScope();
 
-    @Specialization(guards = "getScope().isInt(getSlot())")
+    @Specialization(guards = "frame.isInt(getSlot())")
     protected int readInt(VirtualFrame frame) throws UnexpectedResultException {
-        return getScope().getInt(getSlot());
+        return frame.getInt(getSlot());
     }
 
     @Specialization(guards = "frame.isBoolean(getSlot())")
     protected boolean readBoolean(VirtualFrame frame) {
-        return getScope().getBoolean(getSlot());
+        return frame.getBoolean(getSlot());
     }
 
-    @Specialization(replaces = {"readInt, readLong"}) //TODO work ?
+    @Specialization(replaces = {"readInt, readLong"})
     protected Object readObject(VirtualFrame frame) {
-        if (!getScope().isObject(getSlot())) {
+        if (!frame.isObject(getSlot())) {
             CompilerDirectives.transferToInterpreter();
-            Object result = getScope().getValue(getSlot());
-            getScope().setObject(getSlot(), result);
+            Object result = frame.getValue(getSlot());
+            frame.setObject(getSlot(), result);
 
             return result;
         }
 
-        return getScope().getObject(getSlot());
+        return frame.getObject(getSlot());
     }
 }
