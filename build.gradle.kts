159c13ac863f67356bd8e5a5c9bbc75a407a41c6
IgorErin <erin.igore@yandex.ru>
2022-07-19 00:23:53 +0300

    refactor: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
-    java
+    id("java")
+    application
 }
 
 group = "org.example"
@@ -10,18 +10,34 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.antlr:antlr4:4.10.1")
+val jvmArgsList = listOf(
+    "-Djavafx.embed.singleThread=true",
+    "--add-exports=org.graalvm.truffle/com.oracle.truffle.api=ALL-UNNAMED",
+    "--add-exports=org.graalvm.truffle/com.oracle.truffle.api.nodes=ALL-UNNAMED",
+    "--add-exports=org.graalvm.truffle/com.oracle.truffle.api.dsl=ALL-UNNAMED"
+)
 
+application {
+    mainClass.set("truffle.TruffleMain")
+}
+
+dependencies {
     implementation("org.graalvm.truffle:truffle-api:22.1.0.1")
     annotationProcessor("org.graalvm.truffle:truffle-dsl-processor:22.1.0.1")
+    compileOnly("org.graalvm.truffle:truffle-tck:22.1.0.1")
+    implementation("org.graalvm.sdk:graal-sdk:22.1.0.1")
 
-    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
-    testImplementation(platform("org.junit:junit-bom:5.8.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.antlr:antlr4:4.10.1")
+}
+
+tasks.getByName<JavaExec>("run") {
+    jvmArgs("-Dgraalvm.locatorDisabled=true")
 }
 
 tasks.getByName<Test>("test") {
+    jvmArgs("-Dgraalvm.locatorDisabled=true")
     useJUnitPlatform()
 }
