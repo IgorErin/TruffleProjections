b2a1296c6751a875bea781c14fd6cf2370f8b4ec
IgorErin <erin.igore@yandex.ru>
2022-07-21 19:38:57 +0300

    refator: TruffleMain

--- src/main/java/truffle/TruffleMain.java
+++ src/main/java/truffle/TruffleMain.java
@@ -7,25 +7,32 @@ public class TruffleMain {
         try {
             Context context = Context.create("tf");
             for (int i = 0; i < 20; i++) {
-                context.eval("tf", "(define iter (lambda (counter first second) (if (= counter 0) (+ second) (iter (+ counter (- 1)) second (+ first second)))))\n" +
-                        "\n" +
-                        "(define fib (lambda (x) (iter x 0 1)))\n" +
-                        "\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "(fib 20)\n" +
-                        "\n" +
-                        "(define first (now))\n" +
-                        "\n" +
-                        "(fib 20)\n" +
-                        "\n" +
-                        "(define second (now))\n" +
-                        "\n" +
-                        "(println (+ second (- first)))");
+                context.eval("tf", """ 
+                        (define iter
+                            (lambda (counter first second) (
+                            if (= counter 0)
+                                (+ second)
+                                (iter (+ counter (- 1)) second (+ first second))
+                                )
+                            )
+                         )
+                        (define fib (lambda (x) (iter x 0 1)))
+                                                
+                        (fib 20)
+                        (fib 20)
+                        (fib 20)
+                        (fib 20)
+                        (fib 20)
+                        (fib 20)
+                        (fib 20)
+                                                
+                        (define first (now))
+                                                
+                        (fib 20)
+                                                
+                        (define second (now))
+                                                
+                        (println (+ second (- first)))""");
             }
 
         } catch (Exception e) {
